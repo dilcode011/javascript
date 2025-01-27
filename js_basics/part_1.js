afdb529590941e97ce31ad4b53d567d5d4e48a80
dilcode011 <dilpreetsingh055566@gmail.com>
2025-01-27 14:41:50 +0000

    config done

---
+++ js_basics/part_1.js
@@ -0,0 +1,37 @@
+// BASICS
+
+const accoutid=12;
+let accemail="dil@gmail.com";
+var accpass="123456";
+acccity="delhi";
+accouid=1;
+
+//  console.table([accoutid,accemail,accpass,acccity]);
+
+
+"use strict"; //treat all js code as newer version
+
+alert(3+3);      // used in browser not in nodejs
+
+//  console.log(3+3);     
+
+
+let name="dilpreet";     //declare variable (string)
+
+
+let age=19;  //number =>  number 2 to power 53
+
+let ismarried=false; //*boolean
+
+//null => empty value
+//undefined => not declared
+//symbol => unique value
+
+
+//  console.log(typeof (name)); 
+
+//string
+
+
+
+
