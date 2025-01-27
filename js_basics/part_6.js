6a72e5370599c8f66dfcad45457b96cc69fe2d41
dilcode011 <dilpreetsingh055566@gmail.com>
2025-01-27 16:00:17 +0000

    config done

---
+++ js_basics/part_6.js
@@ -0,0 +1,38 @@
+// NUMBER AND MATHS
+//=>DESCRIPTION OF CODE
+
+
+const balance= new Number(200)
+// console.log(balance);         //=>number:200
+// console.log(balance.toString().length);      //=>3
+// console.log(balance.toFixed(2));      //=>200.00
+
+const otherNumber=12233.3434
+// console.log(otherNumber.toPrecision(3));  //=>round off
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString()); //=>1,000,000
+// console.log(hundreds.toLocaleString('en-IN')); //=>10,00,000 (INDIAN)
+
+
+//----------------------------------MATHS-----------------------------------------------//
+//Math is a object
+
+// console.log(Math);     //=>Object [Math] {}
+
+// console.log(Math.abs(5)); //=>[Function: abs]
+// console.log(Math.round(5.55)); //=>6
+// console.log(Math.ceil(4.22));   //=>5   take upper rounded value
+// console.log(Math.floor(4.22));    //=>4  take lower rounded value
+// console.log(Math.max(2,3,4,5,6));    //=>6
+// console.log(Math.min(2,3,4,5,6));    //=>2
+// console.log(Math.random());     //=>random value
+// console.log(Math.random()*10+1);
+// console.log(Math.floor(Math.random()*10+1));  //=>9
+
+
+
+const min=11
+const max=22
+
+// console.log(Math.floor(Math.random() *  (max-min+1)) + min);           //=>21
