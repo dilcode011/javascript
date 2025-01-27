f9ab0352442dd3c5a2f7bdc99976d1bf8d2b3e21
dilcode011 <dilpreetsingh055566@gmail.com>
2025-01-27 14:53:57 +0000

    config done

---
+++ js_basics/part_2.js
@@ -0,0 +1,68 @@
+// COVERSION
+//=>  DESCRIPTION FOR CODE
+
+let scoree=33
+//  console.log(typeof (scoree))   
+//=>number
+
+
+ let score="33"
+//  console.log(typeof (score))   //=>string
+
+
+
+
+//conversion
+  let scoore="33"
+//console.log(typeof (scoore))   
+//=>string 
+
+ let valueIntNumberr =Number(score);
+// console.log(typeof(valueIntNumberr))
+//=>convert string into number 
+
+ let sccore="33abc"
+ let valueIntNumber =Number(sccore);
+//  console.log(typeof(valueIntNumber))
+//  console.log(valueIntNumber);
+// NaN => not a number
+
+
+ let sscore=null;
+//  console.log(typeof(sscore));
+//=>null
+ let vvalueIntNumber =Number(score);
+//  console.log(vvalueIntNumber)
+//=>return value 0
+
+
+
+ let name="dilpreet";
+//  console.log(typeof(name));
+ //=>string 
+ let valueIntNumbeer =Number(name);
+//  console.log(valueIntNumbeer);
+
+//=>NAn 
+//=>true = 1 
+//=>false =0 
+
+
+ let isloggedin=1
+ let booleanisloggedin=Boolean(isloggedin)
+//  console.log(booleanisloggedin);
+
+ let isloggedinn=""
+ let booleanisloggedinn=Boolean(isloggedinn)
+//  console.log(booleanisloggedin);
+//=>1 = true 
+//=> 0 = false 
+//=> "dilpreet" = true
+
+
+// string conversion
+ let someNumber=34;
+ let stringNumber= String(someNumber);
+//  console.log(stringNumber);
+//  console.log(typeof(stringNumber));
+
