ed07c7a0418c7cfb8f8e9fc716296f1422e5f794
Dilpreet singh <dilpreetsingh055566@gmail.com>
2025-01-29 17:30:57 +0000

    config done

---
+++ js_basics/part_9.js
@@ -0,0 +1,17 @@
+// ALERT AND PROMPT
+/*
+ALERT : display an alert msg on the page
+PROMPT : display a dialog box that ask user for some input
+*/
+alert("erorrrrrrr")
+console.log("this is msg");    //=>msg
+console.error("error msg");   //=>error message with red line
+console.warn("warning msg");  //=>warning msg with yellow line
+
+
+
+prompt("pls enter the name");   //=>take input from user
+
+
+
+    //used in browser otherwise error
