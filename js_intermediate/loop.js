c1f60e3fa892c63b4da752e824a56f001e44a531
Dilpreet singh <dilpreetsingh055566@gmail.com>
2025-02-01 12:30:16 +0000

    updated

--- js_intermediate/loop.js
+++ js_intermediate/loop.js
@@ -1,3 +1,50 @@
 // LOOPS
 //=>DESCRIPTION OF CODE
 
+//=>for(intialisation;cond;updation)
+for(let i=1;i<=5;i++){              
+    // console.log("hello");     
+}      
+
+
+// PRINT ALL ODD NUMBER FROM 1 TO 15
+
+for(let i=1;i<=15;i=-2){
+    // console.log(i); 
+}
+// console.log("IN BACKWARD FORM");
+
+for(let a=15;a>=1;a=a-2){
+    // console.log(a);
+}
+
+
+// PRINT ALL EVEN NUMBERS FROM 2 TO 20
+
+for(let b=2;b<=20;b=b+2){
+    // console.log(b);
+}
+
+for(let c=5;c<=50;c=c+5){
+    // console.log(c);
+}
+
+
+
+let n=prompt("enter the number :")
+n=parseInt(n)   //=>CONVERT STRING INTO INT
+for(let d=n;d<=n*10;d=d+n){
+    // console.log(d);
+    
+}
+
+
+
+// NESTED LOOP
+
+for(let e=1;e<=3;e++){
+    for(let f=1;f<=3;f++){
+        console.log(f);
+        
+    }
+}
