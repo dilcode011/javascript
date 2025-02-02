2d7e6a880322ef10bf8ebd3e42737ec2476567f1
Dilpreet singh <dilpreetsingh055566@gmail.com>
2025-02-02 17:57:17 +0000

    CONFIG DONE

---
+++ js_intermediate/obj_literals.js
@@ -0,0 +1,48 @@
+// OBJECT LITERALS
+//=>DESCRIPTION OF CODE
+   
+   //=>used to store keyed collections and complex entities
+   /*
+     property=>(key,value) pair
+       ->object are a collection of properties
+     
+            |STUDENT|(object)
+        NAME    -   "DILPREET"      
+        AGE    -       19      
+        MARKS    -     8.34(CGPA)  
+           
+         ||              || 
+         ||              ||
+         ||              ||
+        KEYS            VALUE
+   
+--------------SYNTAX----------------
+
+NOTE : key:value
+
+const student ={
+   name:"dil", 
+   age:19,
+   marks:8.89,
+   city:"delhi"
+};    
+
+*/
+
+
+// OBJECT
+const student ={
+    name:"dil", 
+    age:19,
+    marks:8.89,
+    city:"delhi"
+ };   
+
+// ARRAY
+let student2=["dil",19,8.89,"delhi"] 
+//=>BOTH ARE SAME BUT OBJECT IS MUCH CLEAR AND WITH KEYS
+//=>WE CAN ALSO USE ARRAY IN OBJECT
+
+
+// console.log(student.name);        //=>TO ACCESS VALUE
+
---
+++ js_intermediate/practice_ques/obj_lit.js
@@ -0,0 +1,15 @@
+/* CREATE AN OBJECT LITERAL FOR THE PROPERTIES OF THREAD/TWITTER POST WHICH INCLUDES-
+    1. USERNAME
+    2. CONTENT
+    3. LIKES
+    4. REPOSTS
+    5. TAGS
+*/
+
+const post={
+    username:"@dilcode011",
+    content:"coding day",
+    likes:500,
+    reposts:6,
+    tags:["@dilpreet12","@dil21","@desilaw"]
+};
