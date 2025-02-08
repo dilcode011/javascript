cf4b7aaaadeee57648f41a99a55e0d2ea7c48a30
Dilpreet singh <dilpreetsingh055566@gmail.com>
2025-02-08 13:13:20 +0000

    done

---
+++ js_intermediate/practice_ques/func_rand.js
@@ -0,0 +1,12 @@
+// CREATING A FUNCTION TO ROLL A DICE AND ALWAYS DISPLAY THE VALUE OF THE DICE (1 TO 6)
+
+function dice(){
+let num=Math.floor(Math.random()*6)+1
+console.log(num);
+}
+dice()
+dice()
+dice()
+dice()
+dice()
+dice()
