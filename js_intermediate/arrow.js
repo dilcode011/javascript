df8aa3d7b85044ce53a240a370bac3dd8858a990
Dilpreet singh <dilpreetsingh055566@gmail.com>
2025-02-14 11:24:37 +0000

    updated

--- js_intermediate/arrow.js
+++ js_intermediate/arrow.js
@@ -23,7 +23,7 @@ function getavg(){
 
 // TRY AND CATCH
 
-console.log(a);    //IT WILL TRHOUGH ERROR BECUZ IT IS NOT DEFINED
+// console.log(a);    //IT WILL TRHOUGH ERROR BECUZ IT IS NOT DEFINED
 
 
 try {
@@ -66,7 +66,7 @@ const sum=(a,b)=>{
 
 console.log("hello");
 setTimeout(()=>{                 //()=>    IT IS ARROW FUNCTION
-    console.log("hello dil");
+    // console.log("hello dil");
     
 },4000);        //TIME IS IN MILISECOND
 console.log("welcome");
@@ -80,17 +80,17 @@ console.log("welcome");
     */
 
 setInterval(()=>{
-    console.log("hello dil");
+    // console.log("hello dil");
 },2000);
 
 
 // TO TERMINATE THE STATEMENT
 
 let id=setInterval(()=>{
-    console.log("hello dil");
+    // console.log("hello dil");
 },2000);
 
-console.log(id);
+// console.log(id);
 console.log(clearInterval(id));
 
 
@@ -125,4 +125,60 @@ const stu={
 
 }
 
+
+// EVERY
+    //=>RETURN TRUE IF EVERY ELEMENT OF ARRAY GIVES TRUE FOR SOME FUNCTION.ELSE RETURNS FALSE
+    /* 
+        arr.every(function definition or name)
+    */
+
+array3=[2,4,6]
+array3.every((ele)=>(ele%2==0));
+// console.log(array3);
+
+
+// REDUCE
+    //=>REDUCE THE ARRAY TO A SINGLE VALUE
+    /* 
+        arr.reduce(reducer function with 2 variable for(accumulator,element))
+    */
+
+let numbers1=[1,2,3,4]
+let finalValue=numbers1.reduce((res,elem)=>res+elem);
+// console.log(finalValue);       //10
+
+/*
+   (0,1)  -->  1
+   (1,2)  -->  3
+   (3,3)  -->  6
+   (6,4)  -->  10(FinalValue)
+*/
+
+
+// DEFAULT PARAMETERS
+    //=>GIVING A DEFAULT VALUE TO THE ARGUMENTS
+    /*
+       function func(a,b=2){
+            code...
+       }
+    */
+
+function summ(a,b=3){
+    return a+b;
+}
+// console.log(summ(2));
+
+
+// SPREAD
+    //=>EXPANDS AN ITERABLE INTO MULTIPLE VALUES
+    //=>IT CAN BE USED WITH ARRAY LITERALS AND OBJECT LITERALS
+    /*
+       function func(...arr){
+            code..
+       }
+    */
+
+let ArrayOne=[1,2,3,4,1,2,3,2,1,3,2,4,2,1,3]
+let total=Math.min(...ArrayOne)
+console.log(total);
 
